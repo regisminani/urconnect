1b3b678cff80d79d1fff57292115a2c800e6e357
Herve Sugira <herveonly34@gmail.com>
2025-02-13 12:49:27 +0200

    matched the font

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -73,14 +73,14 @@ const LoginPage = () => {
   // };
 
   return (
-    <div className="ml-70 mt-16 w-[700px] h-[500px] flex rounded-10xl relative shadow-2xl shadow-cyan-950 mb-10 font-serif" style={{
+    <div className="ml-70 mt-16 w-[700px] h-[500px] flex rounded-10xl relative shadow-2xl shadow-cyan-950 mb-10 font-inter" style={{
       borderTopLeftRadius: "10px",
       borderBottomLeftRadius: "10px",
       borderBottomRightRadius:"10px",
       borderTopRightRadius:"10px"
     }}>
       <div
-        className="w-1/2 flex flex-col items-center justify-center border-0"
+        className="w-1/2 flex flex-col items-center font-inter justify-center border-0"
         style={{
           borderTopLeftRadius: "10px",
           borderBottomLeftRadius: "10px",
@@ -88,7 +88,7 @@ const LoginPage = () => {
       >
         <div
           style={{ color: "#006991" }}
-          className="text-5xl mt-10 text-center font-serif"
+          className="text-5xl mt-10 text-center"
         >
           UR Connect
         </div>
@@ -119,11 +119,11 @@ const LoginPage = () => {
           X
         </button> */}
 
-        <div className="text-white text-5xl mt-20 font-serif">Student Page</div>
-        <div className="text-white text-3xl mt-7 font-serif">Sign in</div>
+        <div className="text-white text-5xl mt-20 font-inter">Student Page</div>
+        <div className="text-white text-3xl mt-7 font-inter">Sign in</div>
 
         {/* Reg No Input */}
-        <label>
+        <label className="font-inter">
           <input
             className="focus-visible:outline-0 w-full px-13 mt-7 text-gray-700 rounded-lg p-2 bg-white"
             type="text"
@@ -153,10 +153,10 @@ const LoginPage = () => {
 
         {/* Links */}
         <div className="flex justify-between w-full text-sm mt-2">
-          <a href="/forgotPassword" className="text-white hover:underline mt-3">
+          <a href="/forgotPassword" className="text-white font-inter hover:underline mt-3">
             Forgot Password?
           </a>
-          <a href="/signup" className="text-white hover:underline mt-3">
+          <a href="/signup" className="text-white font-inter hover:underline mt-3">
             Sign up
           </a>
         </div>
