52471144c67ca20ff9c6168b86554faa9778b1ad
ll_git <minareg37@gmail.com>
2025-02-27 20:24:33 +0200

    bug fixes, mysuggestions improvement

--- src/components/MySuggestions.tsx
+++ src/components/MySuggestions.tsx
@@ -1,12 +1,38 @@
-import SuggestionAlt from "./SuggestionAlt"
+import useMySuggestions from "../hooks/useMySuggestions";
+import Suggestion from "./Suggestion";
+import SuggestionAlt from "./SuggestionAlt";
 
 const MySuggestions = () => {
-    return (
-        <div>
-            <p className="font-medium text-lg mb-10">My suggestions</p>
-            <SuggestionAlt ms/>
-        </div>
-    )
-}
+  let { loading, error, suggestions } = useMySuggestions();
+  return (
+    <div>
+      <p className="font-medium text-lg mb-10">My suggestions</p>
+      {error && <p className="text-sm text-red-500">{error}</p>}
+      {loading &&
+        Array(5)
+          .fill("")
+          .map((_) => (
+            <div className="bg-neutral-200 animate-pulse rounded-xl w-full h-44 mb-5" />
+          ))}
+      {suggestions.map((s) => (
+        <Suggestion
+          key={s._id}
+          _id={s._id}
+          content={s.content}
+          tags={s.tags}
+          views={s.views}
+          by={s.by}
+          votes={s.votes}
+          status={s.status}
+          comments={s.comments}
+          replies={s.replies}
+          reports={s.reports}
+          createdAt={s.createdAt}
+        />
+      ))}
+      <SuggestionAlt ms />
+    </div>
+  );
+};
 
-export default MySuggestions
+export default MySuggestions;
